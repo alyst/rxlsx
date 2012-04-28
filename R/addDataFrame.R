fc2cf1dc92a2633867cc8b7ddc10154a3cb40c94
Alexey Stukalov <AStukalov@cemm.oeaw.ac.at>
2012-04-28 13:28:02 +0200

    R wrappers for other RCellBlock methods

--- R/addDataFrame.R
+++ R/addDataFrame.R
@@ -102,3 +102,61 @@ addDataFrame <- function(x, sheet, col.names=TRUE, row.names=TRUE,
   return(cellBlock)
 }
 
+setCellStyle <- function( cellBlock, style )
+{
+    cellBlock$setCellStyle( style )
+    invisible()
+}
+
+setCellStyle <- function( cellBlock, style, rowIndex = NULL, colIndex = NULL )
+{
+    cellBlock$setCellStyle( style,
+                            .jarray( as.integer( rowIndex-1L ) ),
+                            .jarray( as.integer( colIndex-1L ) ) )
+    invisible()
+}
+
+setFill <- function( cellBlock, fill, rowIndex = NULL, colIndex = NULL )
+{
+    isXSSF <- .jcall( cellBlock, 'Z', 'isXSSF' )
+    .jcall( cellBlock, 'V', 'setFill',
+            .Rcolor2XLcolor( fill$foregroundColor, isXSSF ), .Rcolor2XLcolor( fill$backgroundColor, isXSSF ),
+            .jshort(FILL_STYLES_[[fill$pattern]]),
+            .jarray( as.integer( rowIndex-1L ) ), .jarray( as.integer( colIndex-1L ) ) )
+    invisible()
+}
+
+setFont <- function( cellBlock, font, rowIndex = NULL, colIndex = NULL )
+{
+    cellBlock$setFont( font$ref,
+                       .jarray( as.integer( rowIndex-1L ) ),
+                       .jarray( as.integer( colIndex-1L ) ) )
+    invisible()
+}
+
+putBorder <- function( cellBlock, border, rowIndex = NULL, colIndex = NULL )
+{
+    isXSSF <- .jcall( cellBlock, 'Z', 'isXSSF' )
+    border_none <- BORDER_STYLES_[['BORDER_NONE']]
+    borders <- c( TOP = border_none, BOTTOM = border_none,
+                  LEFT = border_none, RIGHT = border_none )
+    null_color <- .jnull('org/apache/poi/xssf/usermodel/XSSFColor')
+    border_colors <- c( TOP = null_color, BOTTOM = null_color,
+                        LEFT = null_color, RIGHT = null_color )
+    borders[ border$position ] <- sapply( border$pen, function( pen ) BORDER_STYLES_[pen] )
+    border_colors[ border$position ] <- .Rcolor2XLcolor( border$color, isXSSF )
+    
+    .jcall( cellBlock, "V", "putBorder",
+            .jshort(borders[['TOP']]), border_colors[['TOP']],
+            .jshort(borders[['BOTTOM']]), border_colors[['BOTTOM']],
+            .jshort(borders[['LEFT']]), border_colors[['LEFT']],
+            .jshort(borders[['RIGHT']]), border_colors[['RIGHT']],
+            .jarray( as.integer( rowIndex-1L ) ), .jarray( as.integer( colIndex-1L ) ) )
+    invisible()
+}
+
+getCell <- function( cellBlock, rowIndex, colIndex )
+{
+    return ( .jcall( cellBlock, 'Lorg/apache/poi/ss/usermodel/Cell;', 'getCell',
+              rowIndex - 1L, colIndex - 1L ) )
+}
